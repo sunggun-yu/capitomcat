e04139c5b78b06e57b2f5f5b33ed22e8bcfdc741
Sunggun Yu <sunggun.yu@gmail.com>
2014-01-06 16:16:16 -0500

    Removing params from the task method. : change to using local variable and fetch()

--- lib/capitomcat.rb
+++ lib/capitomcat.rb
@@ -2,30 +2,46 @@ require 'capistrano'
 
 # Author:: Sunggun Yu
 
-desc "Capistrano library for Capitomcat Recipe"
+# Capistrano library for Capitomcat Recipe'
 namespace :capitomcat do
 
-  desc "Start Tomcat server"
-  def startTomcat tomcat_command
-    execute "echo `nohup sudo #{tomcat_command} start&` && sleep 1"
+  # Start Tomcat server
+  def start_tomcat
+    # local variables
+    tomcat_cmd = fetch(:tomcat_cmd)
+
+    puts capture(:sudo, get_sudo_user_tomcat_cmd, "#{tomcat_cmd} start")
   end
 
   # Check status whether started
-  def checkTomcatStarted tomcat_port
-    execute "for i in {0..180}; do echo \"Waiting for Tomcat to start\"; if [ \"\" != \"$\(netstat -an | grep #{tomcat_port}\)\" ]; then break; fi; sleep 30; done"
-    execute "netstat -an | grep #{tomcat_port}"
+  def check_tomcat_started
+    # local variables
+    tomcat_port = fetch(:tomcat_port)
+
+    execute "for i in {0..180}; do echo \"Waiting for Tomcat to start\"; if [ \"\" != \"$(netstat -an | grep #{tomcat_port})\" ]; then break; fi; sleep 30; done"
+    puts capture("netstat -an | grep #{tomcat_port}")
   end
 
   # Stop Tomcat server
-  def stopTomcat tomcat_command
-    execute :sudo, "#{tomcat_command} stop"
+  def stop_tomcat
+    # local variables
+    tomcat_cmd = fetch(:tomcat_cmd)
+
+    puts capture(:sudo, get_sudo_user_tomcat_cmd, "#{tomcat_cmd} stop")
   end
 
   # Upload the WAR file
-  def uploadWarFile upload_user, local_war_file, remote_docBase, tomcat_user, tomcat_user_group
+  def upload_war_file
+    # local variables
+    upload_user = fetch(:user)
+    local_war_file = fetch(:local_war_file)
+    tomcat_war_file = fetch(:tomcat_war_file)
+    tomcat_user = fetch(:tomcat_user)
+    tomcat_user_group = fetch(:tomcat_user_group)
+
     # Setup file name
     temp_dir = Pathname.new('/tmp')
-    temp_file = File.basename(remote_docBase)
+    temp_file = File.basename(tomcat_war_file)
     tmp_war_file = temp_dir.join(temp_file)
 
     # Clean remote file before uploading
@@ -33,63 +49,97 @@ namespace :capitomcat do
     # Upload WAR file into temp dir
     upload! local_war_file, tmp_war_file
     # Move tmp WAR file to actual path
-    moveAndChangeOwner(tmp_war_file, remote_docBase, tomcat_user, tomcat_user_group)
+    move_and_change_owner(tmp_war_file, tomcat_war_file, tomcat_user, tomcat_user_group)
   end
 
   # Generate context.xml file string from ERB template file and bindings
-  def getContextTemplate context_template_file, context_name, remote_docBase
+  def get_context_template
+    # local variables
+    context_template_file = fetch(:context_template_file)
+
+    # local variables for erb file
+    tomcat_context_name = fetch(:tomcat_context_name)
+    tomcat_war_file = fetch(:tomcat_war_file)
+    info ("#{tomcat_context_name}, #{tomcat_war_file}" )
+
     template_file = File.read(File.expand_path(context_template_file, __FILE__))
-    context_name = context_name
-    remote_docBase = remote_docBase
     template = ERB.new(template_file)
     return template.result(binding)
   end
 
   # Upload context template string to remote server
-  def uploadContext upload_user, context_template, remote_context_file, tomcat_user, tomcat_user_group
-    temp_upload_file = '/tmp/' + File.basename(remote_context_file)
+  def upload_context_file
+    # local variables
+    upload_user = fetch(:user)
+    tomcat_context_file = fetch(:tomcat_context_file)
+    tomcat_user = fetch(:tomcat_user)
+    tomcat_user_group = fetch(:tomcat_user_group)
+    context_template = get_context_template
+
+    temp_upload_file = '/tmp/' + File.basename(tomcat_context_file)
     remove_file_if_exist upload_user, temp_upload_file
     contents = StringIO.new(context_template)
     upload! contents, temp_upload_file
-    ChangeOwnerAndMove temp_upload_file, remote_context_file, tomcat_user, tomcat_user_group
+    change_owner_and_move(temp_upload_file, tomcat_context_file, tomcat_user, tomcat_user_group)
   end
 
-  # Clean-up tomcat's work directory
-  def cleanWorkDir remote_tomcat_work_dir, tomcat_user
-    execute "if [ -e #{remote_tomcat_work_dir} ]; then sudo -u #{tomcat_user} rm -rf #{remote_tomcat_work_dir}; fi"
+  # Clean-up work directory
+  def cleanup_work_dir
+    # local variables
+    tomcat_work_dir = fetch(:tomcat_work_dir)
+    tomcat_user = fetch(:tomcat_user)
+
+    execute "if [ -e #{tomcat_work_dir} ]; then sudo -u #{tomcat_user} rm -rf #{tomcat_work_dir}; fi"
   end
 
   # Get Parallelism
-
+  # @return :parallel or :sequence
   def get_parallelism
-    if fetch(:isParallel) == true
+    if fetch(:use_parallel) == true then
       return :parallel
     else
       return :sequence
     end
   end
 
-  # Move file and change owner
+  # Get sudo user for tomcat command
+  # @return -u and sudo user name for tomcat command. if :use_tomcat_user_cmd is false, it will return '-u root'
+  def get_sudo_user_tomcat_cmd
+    if fetch(:use_tomcat_user_cmd) == true then
+      return "-u #{fetch(:tomcat_user)}"
+    else
+      return '-u root'
+    end
+  end
 
-  def moveAndChangeOwner file, destination, user, group
-    # Move tmp WAR fiel to actual path
-    execute :mv, "-f", file, destination
+  # Move file and change owner
+  # @param [String] file
+  # @param [String] destination
+  # @param [String] user
+  # @param [String] group
+  def move_and_change_owner (file, destination, user, group)
+    # Move tmp WAR file to actual path
+    execute :mv, '-f', file, destination
     # Change uploaded WAR file's owner
     execute :sudo, :chown, "#{user}:#{group}", destination
   end
 
   # Change owner and move file
-
-  def ChangeOwnerAndMove file, destination, user, group
+  # @param [String] file
+  # @param [String] destination
+  # @param [String] user
+  # @param [String] group
+  def change_owner_and_move (file, destination, user, group)
     # Change uploaded WAR file's owner
     execute :sudo, :chown, "#{user}:#{group}", file
-    # Move tmp WAR fiel to actual path
-    execute :sudo, "-u", user, :mv, "-f", file, destination
+    # Move tmp WAR file to actual path
+    execute :sudo, '-u', user, :mv, '-f', file, destination
   end
 
   # Remove file if exist
-
-  def remove_file_if_exist user, file
+  # @param [String] user
+  # @param [String] file
+  def remove_file_if_exist (user, file)
     execute "if [ -e #{file} ]; then sudo chown #{user} #{file} ; rm -f #{file}; fi"
   end
 end
